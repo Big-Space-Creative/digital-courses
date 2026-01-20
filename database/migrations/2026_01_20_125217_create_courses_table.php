a9515b7e59507caa37f3cf6c9f91aa5ab7d14cff
JotaEfi <joaodaviabc@gmail.com>
2026-01-20 10:32:52 -0300

    feat: modulação do banco de dados e organização dos arquivos de enviroment_variables para melhor acesso.

---
+++ database/migrations/2026_01_20_125217_create_courses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title', 255);
+            $table->string('slug', 255);
+            $table->text('description', 255)->nullable();
+            $table->decimal('price', 8, 2)->default(0.00);
+            $table->string('thumbnail', 255);
+            $table->boolean('is_published');
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+    }
+};
